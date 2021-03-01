ba5131d60041891a2567cd111eab064269b0e550
intrnl <intrnl28+git@gmail.com>
2021-03-01 10:42:05 +0700

    chore: fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from './lib/vite-plugin-svelte.js';
 
 export default defineConfig({
 	root: 'src/',
+	base: '/pso2-daily-viewer/',
 	build: {
 		outDir: '../dist/',
 		emptyOutDir: true,
